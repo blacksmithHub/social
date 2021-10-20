83970f36247fafc45fc63d610a4feb84782d534b
Danniel Isiah J. Libor <disaiahlibor@gmail.com>
2021-10-20 19:35:28 +0800

    initial profile

--- services/profile-api/database/seeders/ProfileSeeder.php
+++ services/profile-api/database/seeders/ProfileSeeder.php
@@ -2,12 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Profile;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ProfileSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
